472ea3ccd889731310277342b25a3e36ae831a08
Leonardo M. de Oliveira <leonardomendoncakta@gmail.com>
2022-08-06 12:30:24 -0300

    teste api

--- App.js
+++ App.js
@@ -3,8 +3,19 @@ import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
 import { Text, View, StyleSheet } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const express = require('express');
+const server = express();
+const dummyData = require('./src/data/dummyData.json');
+
+server.get('/api', (req,res) => {
+    return res.json({dummyData})
+});
+
+server.listen(3000, () => {
+    console.log('Servidor está funcionando...')
+});
 
 const Stack = createNativeStackNavigator ()
 
