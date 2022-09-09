303ede8137e6be939447d165242118adfb1a271c
Hilton <hiltoncarrara003@gmail.com>
2022-09-09 15:54:37 -0300

    Login, Splash e Perfil (Ajustar)

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Routes from './src/routes';
+import Login from './src/pages/login';
+import Splash from './src/pages/splash';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator()
+
 
 export default function App() {
   return (    
-    
     <NavigationContainer>
-      <Routes/>
+      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash">
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="TabBar" component={Routes} />
+      </Stack.Navigator>
+    {/*<Routes/>*/}
     </NavigationContainer>
     
+  );
+}
+// Criar sistema de verificação de login
+export function tabBar() {
+  return (    
+    <NavigationContainer>
+    <Routes/>
+    </NavigationContainer>
   );
 }
---
+++ src/pages/login.js
@@ -0,0 +1,109 @@
+import React, { useEffect } from 'react';
+import { View, Text, BackHandler, Image, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import * as AuthSession from 'expo-auth-session';
+import Splash from '../pages/splash';
+
+export default function Login() {
+    const navigation = useNavigation();
+
+    //Comando utilizado através do useEffect para evitar que a animação da tela splash se repita após o usuário usar o back.
+    useEffect(() => {
+        BackHandler.addEventListener('hardwareBackPress', () => {
+            return true
+        })
+    }, [])
+
+    //Função assíncrona criada para passar os parâmetros necessários para o login via Google.
+    async function handleSignIn() {
+
+        const CLIENT_ID = '1059836562881-1trgti4aldugcah36s0fi2tm9vhef1gj.apps.googleusercontent.com';
+        const REDIRECT_URI = 'https://auth.expo.io/@guilhermecod/InvestMedia';
+        const RESPONSE_TYPE = 'token';
+        const SCOPE = 'profile email';
+
+
+        const authUrl = encodeURI(`https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`);
+        console.log(authUrl);
+        const response = await AuthSession.startAsync({ authUrl });
+
+        console.log(response);
+
+        navigation.navigate('TabBar');
+
+    }
+
+    return (
+        //Imagem da logo do InvestMedia e botão personalizado do login com o Google.
+        // Parte do código que permite a utilização de uma imagem no botão.
+        <SafeAreaView style={{ flex: 1 }}>
+            <View style={styles.fundo}>
+                <Image style={styles.ImagemLogin}
+                    source={require('../../assets/LoginImage.png')}
+                />
+    
+                <TouchableOpacity
+
+                    onPress={handleSignIn}
+                    style={styles.botaogoogle}
+                    activeOpacity={0.5}>
+                    <Image
+                        source={require('../../assets/googlebutton.png')}
+                        style={styles.imagembotaogoogle}
+                    />
+                    <View style={styles.separadorbotao} />
+                    <Text style={{ color: '#808080', marginBottom: 4, marginLeft: 48, fontWeight: "bold" }}>
+                        Continuar com o Google
+                    </Text>
+                    
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
+
+
+//Estilos utilizados nessa tela.
+
+const styles = StyleSheet.create({
+    fundo: {
+        flex: 1,
+        backgroundColor: '#f5f5f6',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    ImagemLogin: {
+        width: 350,
+        resizeMode: 'contain',
+        marginTop: 90,
+
+    },
+    botaogoogle: {
+
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF',
+        borderWidth: 5,
+        borderColor: '#fff',
+        height: 55,
+        borderRadius: 30,
+        margin: 5,
+        width: 300,
+        marginTop: 10,
+        marginBottom: 410,
+    },
+    imagembotaogoogle: {
+
+        padding: 10,
+        margin: 5,
+        height: 25,
+        width: 25,
+        resizeMode: 'stretch',
+    },
+    separadorbotao: {
+
+        backgroundColor: '#fff',
+        width: 1,
+        height: 40,
+    },
+});
