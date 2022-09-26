7192b2880e0cca47846bec17b3f1f937d2fe8ea0
Hilton <hiltoncarrara003@gmail.com>
2022-09-26 10:42:57 -0300

    tela de pesquisa - Revisar

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,22 +1,100 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { Text, View, StyleSheet, FlatList, SafeAreaView, TextInput } from 'react-native';
 
-export default function Search() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}> Pagina pesquisa</Text>
-    </View>
-  );
+const Search = () => {
+  const [filterdData, setfilterdData] = useState ([]);
+  const [masterData, setmasterData] = useState ([]);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    fetchPosts();
+    return () => {
+
+    }
+  }, [])
+
+  const fetchPosts = () => {
+    const apiURL = 'https://jsonplaceholder.typicode.com/posts';
+    fetch(apiURL)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      setfilterdData(responseJson);
+      setmasterData(responseJson);
+    }).catch((error) => {
+      console.error(error);
+    })
+  }
+}
+
+const searchFilter = (text) => {
+  if(text){
+    const newData = masterData.filter((item) => {
+      const itemData = item.title ? item.title.toUpperCase()
+        : ''.toUpperCase();
+      const textData = text.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    setfilterdData(newData);
+    setsearch(text);
+  }else {
+    setfilterdData(masterData);
+    setsearch(text);
+  }
+}
+
+export default function SearchScreen() {
+  return(
+    <SafeAreaView style={{flex:1}}>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInputStyle}
+          value={search}
+          placeholder="Search here"
+          underlineColorAndroid="transparent"
+          onChangeText={(text) => searchFilter(text)}
+        />
+        <FlatList
+          data={filterdData}
+          keyExtactor={(item, index) => index.toString()}
+          ItemSeparatorComponent={ItemSeparatorView}
+          renderItem={ItemView}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const ItemView = ({item}) => {
+  return(
+    <Text style={styles.itemStyle}>
+      {item.id}{'. '}{item.title.toUpperCase()}
+    </Text>
+  )
+}
+
+const ItemSeparatorView = () => {
+  return(
+    <View
+      style={{height:0.5, width:'100%', backgroundColor:'#c8c8c8'}}
+    />
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: 'white',
   },
-  text: {
-    fontSize: 25,
-    fontWeight: 'bold',
+  itemStyle: {
+    padding: 15
+  },
+  textInputStyle: {
+    height: 50,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: '#009688',
+    backgroundColor: 'white'
   },
 });
+
+
