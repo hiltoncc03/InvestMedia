ed3d48c36cd0d5bfac589f9b7ea7c61d9509e6fe
Hilton <hiltoncarrara003@gmail.com>
2022-10-11 09:51:07 -0300

    Pesquisa de Usuários - Terminado

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,36 +4,51 @@ import { useNavigation } from '@react-navigation/native';
 
 const ListItem = ({ data }) => {
   const navigation = useNavigation()
+  console.log("listItem")
+  console.log(data)
   return (
     <TouchableOpacity style={styles.item} onPress={() =>{
         navigation.navigate('TabBar', {'USER_ID' : data.id});
       }}>
-      <Image source={{ uri: data.fotoPerfil }} style={styles.itemPhoto} />
-      <View style={styles.itemInfo}>
+      <View style = {styles.container}>
+        <Image source={{ uri: data.fotoPerfil }} style={styles.itemPhoto} />
+        <View style={styles.itemInfo}>
         <Text style={styles.itemP1}>{data.userName}</Text>
         <Text style={styles.itemP2}>{data.nome}</Text>
+        </View>
       </View>
     </TouchableOpacity>
+    
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F5F5F5',
+    flexDirection: 'row',
+    flex: 1,
+    borderRadius: 10
+  },
   item: {
     flexDirection: 'row',
-    marginLeft: 30,
-    marginRight: 30,
-    borderBottomWidth: 1,
-    borderBottomColor: '#444',
-    paddingTop: 15,
-    paddingBottom: 15,
+    marginLeft: 10,
+    marginRight: 10,
+    paddingTop: 10,
+    backgroundColor: 'fff',
+    justifyContent: 'center',
   },
   itemPhoto: {
     width: 50,
     height: 50,
     borderRadius: 30,
+    marginLeft: 15,
+    marginTop: 10,
+    marginBottom: 10
   },
   itemInfo: {
-    marginLeft: 20,
+    marginLeft: 13,
+    backgroundColor: 'fff',
+    padding: 5
   },
   itemP1: {
     fontSize: 22,
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -13,37 +13,43 @@ import ListItem from '../../components/ListItem';
 
 const App = () => {
   const [searchText, setSearchText] = useState('');
-  const baseUrl =  'https://investmedia-server.glitch.me/searchUser';
-  const [list, setList] = useState(baseUrl);
-  axios.get(`${baseUrl}/${searchText}`).then(function(response){
-    console.log('Success: ', response.data);    
-    useEffect(() => {
-      if (searchText === '') {
-        setList(response.data);
-      } else {
-        setList(
-          response.filter(item => {
-            item.data.nome.toLowerCase().indexOf(searchText.toLowerCase()) > -1
-          })
+  const [canSearch, setCanSearch] = useState(true);
+  const [delayTimeout, setDelayTimeout] = useState(() => { })
+  const baseUrl = 'https://investmedia-server.glitch.me/searchUser';
+  const [list, setList] = useState([]);
+
+  useEffect(function () {
+    if (canSearch) {
+      setCanSearch(false)
+      axios.get(`${baseUrl}/${searchText}`)
+        .then((response) => {
+          console.log('Success: ', response.data);
+          setList(
+            response.data
           );
-        }
-      }, [searchText]);
-        
-      }).catch(function(error){
-        console.log('Error: ', response.data)
-      });
-  
-  
+
+        }).catch(function (error) {
+          console.log('Error: ', error)
+        })
+    }
+  }, [searchText, canSearch])
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.searchArea}>
         <TextInput
           style={styles.input}
-          placeholder="Pesquise uma pessoa"
+          placeholder="Pesquisar"
           placeholderTextColor="#888"
           value={searchText}
-          onChangeText={(t) => setSearchText(t)}
-          />
+          onChangeText={(t) => {
+            setSearchText(t);
+
+            setDelayTimeout(setTimeout(() => {
+              setCanSearch(true);
+            }, 750));
+          }}
+        />
       </View>
 
       <FlatList
@@ -51,7 +57,7 @@ const App = () => {
         style={styles.list}
         renderItem={({ item }) => <ListItem data={item} />}
         keyExtractor={(item) => item.id}
-        />
+      />
 
       <StatusBar style='light' />
     </SafeAreaView>
@@ -59,7 +65,7 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-   container: {
+  container: {
     flex: 1,
     backgroundColor: '#E1E2E1',
   },
@@ -68,12 +74,11 @@ const styles = StyleSheet.create({
     height: 50,
     backgroundColor: '#F5F5F5',
     margin: 10,
-    marginTop: 25,
-    borderRadius: 30,
+    marginTop: 15,
+    borderRadius: 10,
     fontSize: 19,
-    paddingLeft: 5,
-    paddingRight: 5,
     color: '#000000',
+    padding: 10
   },
   searchArea: {
     marginLeft: 5,
