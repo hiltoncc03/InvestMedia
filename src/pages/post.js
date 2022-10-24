e8fa6375cacdb74fdf220e433b2c82dbc21b2fc7
Hilton <hiltoncarrara003@gmail.com>
2022-10-24 15:17:56 -0300

    Atualizando

--- src/pages/post.js
+++ src/pages/post.js
@@ -2,7 +2,6 @@ import React, { FC, ReactElement, useState } from "react";
 import {
   Button,
   StyleSheet,
-  TextInput,
   TouchableOpacity,
   Text,
   Image,
@@ -11,38 +10,42 @@ import {
 import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
 import axios from "axios";
+import { TextInput } from 'react-native-paper';
 
 export default function Post({ route }) {
   const navigation = useNavigation();
   const userInfo = route.params.userInfo[0]
   console.log(userInfo);
-  const [userName, setUsername] = useState("");
   const [userBio, setUserBio] = useState("");
 
   return (
-    <View style={styles.screenView}>
-      <Image style={styles.fotoPost} source={{uri: userInfo.fotoPerfil}}></Image>
-      {console.log(userName)}
-      <TextInput
-        style={styles.inputBio}
-        value={userBio}
-        placeholder={"Nos conte sobre você: "}
-        maxLength={100}
-        multiline
-        numberOfLines={4}
-        selectionColor="#CFB43C"
-        onChangeText={(text) => setUserBio(text)}
-      />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => {
-          
-        }}
-      >
-        <Text style={{ color: "white", alignSelf: "center", padding: 7 }}>
-          Confirmar
-        </Text>
-      </TouchableOpacity>
+    <View style={styles.screenViewAll}>
+      <View style={styles.screenView}>
+        <Image style={styles.fotoPost} source={{ uri: userInfo.fotoPerfil }}></Image>
+        <TextInput
+          style={styles.input}
+          value={userBio}
+          label= "Compartilhe algo com seus seguidores: "
+          maxLength={300}
+          multiline
+          numberOfLines={4}
+          selectionColor="#CFB43C"
+          textBreakStrategy= 'highQuality'
+          onChangeText={(text) => setUserBio(text)}
+        />
+      </View>
+      <View style={styles.screenView2}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+
+          }}
+        >
+          <Text style={{ color: "white", alignSelf: "center", padding: 7 }}>
+            Publicar
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -52,36 +55,41 @@ const styles = StyleSheet.create({
     borderRadius: 999,
     height: 30,
     width: 30,
-    alignSelf: "center",
-    marginTop: "4%",
+    marginRight: 10,
+    alignSelf: 'center'
   },
-  inputUser: {
-    height: 40,
-    width: 350,
-    marginBottom: 10,
+  input: {
+    height: 200,
+    width: 370,
     backgroundColor: "#fff",
     justifyContent: "flex-start",
-    padding: 10,
+    borderRadius: 10
   },
-  inputBio: {
-    height: 90,
-    width: 350,
-    marginBottom: 10,
-    backgroundColor: "#fff",
-    justifyContent: "flex-end",
+  screenViewAll: {
+    flex: 1,
     padding: 10,
+    alignContent: "flex-start",
+    alignSelf: "center",
   },
   screenView: {
-    flex: 1,
+    padding: 10,
     alignContent: "flex-start",
-    alignItems: "center",
-    flexDirection: 'row'
+    alignSelf: "center",
+    flexDirection: 'row',
+    backgroundColor: "blue"
+  },
+  screenView2: {
+    marginTop: 100,
+    padding: 10,
+    alignContent: "flex-start",
+    alignSelf: "center",
+    flexDirection: 'row',
+    backgroundColor: 'red',
   },
   button: {
     borderRadius: 100,
     backgroundColor: "#CFB43C",
     height: 37,
     width: 115,
-    marginTop: 30,
   },
 });
