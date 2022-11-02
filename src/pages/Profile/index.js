1207df6fac046203f5d820ad58ab8e05e19897f4
Hilton <hiltoncarrara003@gmail.com>
2022-11-02 10:32:19 -0300

    Mudanças nas requisições do perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -13,50 +13,55 @@ import {
 } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import axios from "axios";
+import { set } from "react-native-reanimated";
 const Stack = createNativeStackNavigator();
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 const baseUrl = "https://investmedia-server.glitch.me";
 
-const getUserInfo = async (loggedUser) => {
+const getUserInfo = async (showProfileID) => {
+  const response = await axios.get(`${baseUrl}/infoUser/${showProfileID}`);
+  console.log(`${baseUrl}/infoUser/${showProfileID}`);
+  // await delay(3000);
+  return response.data;
+};
+
+const getPosts = async (showProfileID) => {
+  const response = await axios.get(
+    "https://investmedia-server.glitch.me/userPub/" + showProfileID
+  );
+  console.log(response.data);
+  return response.data.reverse();
+};
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("");
+    }, ms);
+  });
+}
+
+//RETORNA O NUMERO DE SEGUIDORES DO USUÁRIO
+const getFollowersNumber = async (showProfileID) => {
   if (showProfileID) {
-    const response = await axios.get(`${baseUrl}/infoUser/${loggedUser}`);
-    console.log(response.data[0]);
-    await delay(3000);
-    
+    const response = await axios.get(
+      `https://investmedia-server.glitch.me/getFollowersNumber/${showProfileID}`
+    );
+    return response.data[0].folowersNumber;
   }
 };
 
-//RETORNA O NUMERO DE SEGUIDORES DO USUÁRIO
-function getFollowersNumber(showProfileID) {
-  const [folowersNumber, setFolowersNumber] = useState([]);
-  if (showProfileID) {
-    axios
-      .get(
-        `https://investmedia-server.glitch.me/getFollowersNumber/${showProfileID}`
-      )
-      .then((response) => {
-        setFolowersNumber(response.data[0].folowersNumber);
-      });
-    return folowersNumber;
-  }
-}
-
 //RETORNA O NUMERO DE PUBLICAÇÕES DO USUÁRIO
-function getPostsNumber(showProfileID) {
+const getPostsNumber = async (showProfileID) => {
   if (showProfileID) {
-    const [postsNumber, setPostsNumber] = useState([]);
-    axios
-      .get(
-        `https://investmedia-server.glitch.me/getPostsNumber/${showProfileID}`
-      )
-      .then((response) => {
-        setPostsNumber(response.data[0].postsNumber);
-      });
-    return postsNumber;
+    response = await axios.get(
+      `https://investmedia-server.glitch.me/getPostsNumber/${showProfileID}`
+    );
+    return response.data[0].postsNumber;
   }
-}
+};
 
 export default function App({ route }) {
   //PASSANDO showProfileID DE "Login.js" PARA "Routes.js" E PARA "Index.js" (PERFIL)
@@ -67,6 +72,9 @@ export default function App({ route }) {
   const [isUserProfile, setIsUserProfile] = useState(false);
   const [dadosPerfil, setDados] = useState([]);
   const [posts, setPosts] = useState([]);
+  const [userInfoRequested, setUserInfoRequested] = useState(false);
+  const [postsNumber, setPostsNumber] = useState(0);
+  const [followersNumber, setFollowersNumber] = useState(0);
   //Faz a requisição das informações do usuário
   useEffect(() => {
     if (loggedUser) {
@@ -76,209 +84,211 @@ export default function App({ route }) {
         setShowProfileID(loggedUser);
       }
       if (showProfileID) {
-        axios
-          .get(`${baseUrl}/infoUser/${showProfileID}`)
-          .then((response) => {
-            console.log(
-              "\n\n\n\n----------------- FAZENDO REQUISIÇÂO ----------------"
-            );
-            setDados(response.data[0]);
-          })
-          .catch((error) => console.log(error.data));
+        if (!userInfoRequested) {
+          async function load() {
+            console.log("FAZENDO REQUISIÇÕES");
+            const data = await getUserInfo(showProfileID);
+            const tempPostsN = await getPostsNumber(showProfileID);
+            const tempFollowers = await getFollowersNumber(showProfileID);
+            const tempPosts = await getPosts(showProfileID);
+
+            setUserInfoRequested(true);
+            if (data) setDados(data[0]);
+            setPostsNumber(tempPostsN);
+            setFollowersNumber(tempFollowers);
+            setPosts(tempPosts);
+            console.log(dadosPerfil);
+          }
+
+          load();
+        }
       }
+      console.log("Usuário conectado:");
+      console.log(loggedUser);
+      console.log("Usuário mostrado");
+      console.log(showProfileID);
     }
   }, [showProfileID]);
 
-  //faz a requisição das informações de publicação do usuário
-  useEffect(() => {
-    axios
-      //.get(`${baseUrl}/userPub/${showProfileID}`)
-      .get("https://investmedia-server.glitch.me/userPub/" + showProfileID)
-      .then((response) => {
-        setPosts(response.data.reverse());
-        console.log(
-          "\n\n\n\n---------------------PUBLICAÇÔES--------------------"
-        );
-        console.log(response.data);
-      })
-      .catch((error) => console.log(error.data));
-  }, [showProfileID]);
+  if (userInfoRequested) {
+    return (
+      <View
+        style={{
+          alignItems: "center",
+          marginLeft: "2%",
+          marginRight: "2%",
+        }}
+      >
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          numColumns={3}
+          horizontal={false}
+          //contentContainerStyle={{alignSelf: 'center'}}
+          //ListEmptyComponent= { }
+          ListHeaderComponent={
+            <View>
+              {/*HEADER DO PERFIL*/}
+              <View style={styles.headerPerfil}>
+                <View style={styles.headerPerfilEsquerdo}>
+                  <Image
+                    source={{
+                      uri: dadosPerfil.fotoPerfil,
+                    }}
+                    style={styles.photoPerfil}
+                  ></Image>
+                  <Text style={styles.textUser}>{dadosPerfil.userName}</Text>
 
-  console.log("Usuário conectado:");
-  console.log(loggedUser);
-  console.log("Usuário mostrado");
-  console.log(showProfileID);
-  console.log("Data");
-  console.log(dadosPerfil);
-
-  return (
-    //AJUSTAR ALINHAMENTO DAS PUBLICAÇÕES
-    <View style={{ alignItems: "center", marginLeft: "2%", marginRight: "2%" }}>
-      <FlatList
-        showsVerticalScrollIndicator={false}
-        numColumns={3}
-        horizontal={false}
-        //contentContainerStyle={{alignSelf: 'center'}}
-        //ListEmptyComponent= { }
-        ListHeaderComponent={
-          <View>
-            {/*HEADER DO PERFIL*/}
-            <View style={styles.headerPerfil}>
-              <View style={styles.headerPerfilEsquerdo}>
-                <Image
-                  source={{
-                    uri: dadosPerfil.fotoPerfil,
-                  }}
-                  style={styles.photoPerfil}
-                ></Image>
-                <Text style={styles.textUser}>{dadosPerfil.userName}</Text>
-
-                <View style={{ flexDirection: "row", alignSelf: "center" }}>
-                  <View style={{ marginRight: 30 }}>
-                    <Text style={styles.textQuant}>
-                      {getPostsNumber(showProfileID)}
-                    </Text>
-                    <Text style={styles.textLegenda}>Publicações</Text>
+                  <View style={{ flexDirection: "row", alignSelf: "center" }}>
+                    <View style={{ marginRight: 30 }}>
+                      <Text style={styles.textQuant}>{postsNumber}</Text>
+                      <Text style={styles.textLegenda}>Publicações</Text>
+                    </View>
+                    <View>
+                      <Text style={styles.textQuant}>{followersNumber}</Text>
+                      <Text style={styles.textLegenda}>Seguidores</Text>
+                    </View>
                   </View>
-                  <View>
-                    <Text style={styles.textQuant}>
-                      {getFollowersNumber(showProfileID)}
-                    </Text>
-                    <Text style={styles.textLegenda}>Seguidores</Text>
+                </View>
+                <View style={styles.headerPerfilDireito}>
+                  <View style={{ justifyContent: "flex-start", height: 82 }}>
+                    <Text style={styles.textName}>{dadosPerfil.nome}</Text>
+                    <Text style={styles.textBio}>{dadosPerfil.userBio}</Text>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <Image
+                      source={require("../../../assets/images/TriânguloUp.png")}
+                      style={styles.iconStock}
+                    />
+                    <Text style={styles.textStock}>6%</Text>
+                    <Image
+                      source={require("../../../assets/images/TriânguloDown.png")}
+                      style={styles.iconStock}
+                    />
+                    <Text style={styles.textStock}>2.3%</Text>
+                  </View>
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      marginTop: 2,
+                      marginRight: 15,
+                    }}
+                  >
+                    <Text style={styles.textLegenda2}>Resumo Mensal</Text>
+                    <Text style={styles.textLegenda2}>Resumo Semanal</Text>
+                  </View>
+                  <View style={{ flexDirection: "row", alignSelf: "center" }}>
+                    <TouchableOpacity
+                      disabled={isUserProfile} //Desativa o botão seguir, se o usuário estiver no próprio perfil
+                      style={
+                        isUserProfile
+                          ? styles.pressableHeaderDisabled
+                          : styles.pressableHeader
+                      }
+                    >
+                      <Text
+                        style={{
+                          textAlign: "center",
+                          color: "#ffffff",
+                          fontSize: 14,
+                          textAlignVertical: "auto",
+                          padding: 3.5,
+                        }}
+                      >
+                        Seguir
+                      </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.pressableHeader}>
+                      <Text
+                        style={{
+                          textAlign: "center",
+                          color: "#ffffff",
+                          fontSize: 14,
+                          textAlignVertical: "auto",
+                          padding: 3.5,
+                        }}
+                      >
+                        Mensagem
+                      </Text>
+                    </TouchableOpacity>
                   </View>
                 </View>
               </View>
-              <View style={styles.headerPerfilDireito}>
-                <View style={{ justifyContent: "flex-start", height: 82 }}>
-                  <Text style={styles.textName}>{dadosPerfil.nome}</Text>
-                  <Text style={styles.textBio}>{dadosPerfil.userBio}</Text>
-                </View>
-                <View style={{ flexDirection: "row" }}>
-                  <Image
-                    source={require("../../../assets/images/TriânguloUp.png")}
-                    style={styles.iconStock}
-                  />
-                  <Text style={styles.textStock}>6%</Text>
-                  <Image
-                    source={require("../../../assets/images/TriânguloDown.png")}
-                    style={styles.iconStock}
-                  />
-                  <Text style={styles.textStock}>2.3%</Text>
-                </View>
+
+              <View style={styles.publicacoesPerfil}>
                 <View
                   style={{
                     flexDirection: "row",
-                    marginTop: 2,
-                    marginRight: 15,
+                    justifyContent: "space-between",
+                    marginRight: 90,
+                    marginLeft: 90,
                   }}
                 >
-                  <Text style={styles.textLegenda2}>Resumo Mensal</Text>
-                  <Text style={styles.textLegenda2}>Resumo Semanal</Text>
-                </View>
-                <View style={{ flexDirection: "row", alignSelf: "center" }}>
-                  <TouchableOpacity
-                    disabled={isUserProfile} //Desativa o botão seguir, se o usuário estiver no próprio perfil
-                    style={
-                      isUserProfile
-                        ? styles.pressableHeaderDisabled
-                        : styles.pressableHeader
-                    }
+                  <Pressable
+                    style={({ pressed }) => [
+                      {
+                        backgroundColor: pressed ? "#D4D4D4" : "white",
+                      },
+                      { width: 19.11, height: 21.06, marginTop: 5 },
+                    ]}
                   >
-                    <Text
+                    <Image
+                      source={require("../../../assets/images/iconePerfilAtivos.png")}
                       style={{
-                        textAlign: "center",
-                        color: "#ffffff",
-                        fontSize: 14,
-                        textAlignVertical: "auto",
-                        padding: 3.5,
+                        width: 19.11,
+                        height: 21.06,
+                        resizeMode: "cover",
                       }}
-                    >
-                      Seguir
-                    </Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity style={styles.pressableHeader}>
-                    <Text
+                    />
+                  </Pressable>
+                  <View
+                    style={{
+                      backgroundColor: "#eaeaea",
+                      width: 2,
+                      height: 25,
+                      fill: 1,
+                      borderRadius: 10,
+                      marginTop: 5,
+                    }}
+                  ></View>
+                  <Pressable
+                    style={({ pressed }) => [
+                      {
+                        backgroundColor: pressed ? "#D4D4D4" : "white",
+                      },
+                      { width: 25.74, height: 20.67, marginTop: 5 },
+                    ]}
+                  >
+                    <Image
+                      source={require("../../../assets/images/iconePerfilFotos.png")}
                       style={{
-                        textAlign: "center",
-                        color: "#ffffff",
-                        fontSize: 14,
-                        textAlignVertical: "auto",
-                        padding: 3.5,
+                        width: 25.74,
+                        height: 20.67,
+                        resizeMode: "cover",
                       }}
-                    >
-                      Mensagem
-                    </Text>
-                  </TouchableOpacity>
+                    />
+                  </Pressable>
                 </View>
               </View>
             </View>
-
-            <View style={styles.publicacoesPerfil}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  marginRight: 90,
-                  marginLeft: 90,
-                }}
-              >
-                <Pressable
-                  style={({ pressed }) => [
-                    {
-                      backgroundColor: pressed ? "#D4D4D4" : "white",
-                    },
-                    { width: 19.11, height: 21.06, marginTop: 5 },
-                  ]}
-                >
-                  <Image
-                    source={require("../../../assets/images/iconePerfilAtivos.png")}
-                    style={{ width: 19.11, height: 21.06, resizeMode: "cover" }}
-                  />
-                </Pressable>
-                <View
-                  style={{
-                    backgroundColor: "#eaeaea",
-                    width: 2,
-                    height: 25,
-                    fill: 1,
-                    borderRadius: 10,
-                    marginTop: 5,
-                  }}
-                ></View>
-                <Pressable
-                  style={({ pressed }) => [
-                    {
-                      backgroundColor: pressed ? "#D4D4D4" : "white",
-                    },
-                    { width: 25.74, height: 20.67, marginTop: 5 },
-                  ]}
-                >
-                  <Image
-                    source={require("../../../assets/images/iconePerfilFotos.png")}
-                    style={{ width: 25.74, height: 20.67, resizeMode: "cover" }}
-                  />
-                </Pressable>
-              </View>
-            </View>
-          </View>
-        }
-        // data={(posts.url =! null ? posts.url : null)}
-        data={posts}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={[styles.publicacaoFeed]}
-            onPress={() => console.log("Publicação " + item.POST_ID)}
-          >
-            {/* {console.log(item.url)} */}
-            <Image
-              source={{ uri: item.midia }}
-              style={styles.publicacaoFeed}
-            ></Image>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
-  );
+          }
+          data={posts}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={[styles.publicacaoFeed]}
+              onPress={() => console.log("Publicação " + item.POST_ID)}
+            >
+              <Image
+                source={{ uri: item.midia }}
+                style={styles.publicacaoFeed}
+              ></Image>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
+    );
+  } else {
+    return <Text> carregando... </Text>;
+  }
 }
 
 const styles = StyleSheet.create({
